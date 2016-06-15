b35bb85a7f4f148a2b791f4f03bd5f9a55bf1340
Joel Pan <spamtastic@ketsugi.com>
2016-06-15 23:46:47 +0800

    Added quotation marks to Exception string

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function required(argumentName){
 	const funcName = required.caller.name ? `"${required.caller.name}"` : "<anonymous function>";
 	let exception = 'Missing argument ';
 	if (argumentName) {
-		exception += `${argumentName} `;
+		exception += `"${argumentName}" `;
 	}
 	exception += `in function ${funcName}`;
 	throw new Error(exception);
